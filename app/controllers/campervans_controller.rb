d3b28e1c92a045bf8af088a90a7c81b1c204b992
Harry Bremner <harrybremner@gmail.com>
2022-06-02 15:40:11 +0100

    some changes made

--- app/controllers/campervans_controller.rb
+++ app/controllers/campervans_controller.rb
@@ -1,7 +1,6 @@
 class CampervansController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-    # @campervans = Campervan.all
     if params[:query].present?
       @campervans = Campervan.search_by_location(params[:query])
     else
